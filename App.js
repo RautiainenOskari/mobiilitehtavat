fb48ff82c16e590a61304fcf05cb93a1b3245397
Oskari Rautiainen <rautiainen.oskarir@gmail.com>
2026-01-15 10:23:35 +0200

    Messing around

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+const [message, setMessage] = useState('');
+
+const handlePress = () => {
+  Alert.alert('You typed:', message);
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+<TextInput
+  placeholder='Enter some text'
+  onChangeText={text => setMessage(text)} 
+  value={message}
+  keyboardType='default' 
+/>
+      <Button onPress={handlePress} title="Press me" />
       <StatusBar style="auto" />
+      <Image
+  style={{ width:250, height: 250, marginTop: 20 }}
+  source={require('./assets/seagull-7282992_1920.png')} 
+/>
     </View>
   );
 }
